3c303f0e44eddfd15f634fe831d18fa62a1a620f
David Zapata <luisdavid@google.com>
2023-02-10 18:20:31 -0500

    tests

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -7,14 +7,14 @@ const singup = {
         try {
             const user = await User.findOne({where:{email}})
             if (user){
-                return res.status(404).json({"msg": "Este usuario ya está registrado", "statusCode": "404"})
+                return res.json({"msg": "Este usuario ya está registrado", "statusCode": "404"})
             }
             await User.create({
                 email,
                 name,
                 password
             })
-            res.json({"status": "OK", "statusCode" : "200"})
+            res.json({"msg": "Usuario creado correctamente", "statusCode" : "200"})
             
         } catch (error) {
             res.json({"statusCode" : "404", "msg" : error.message})
