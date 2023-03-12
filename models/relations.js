a91469d0c0717c8a4cdf4190d5637f626b5c38ee
David Zapata <davidecf@outlook.fr>
2023-03-11 21:35:25 -0500

    test

--- models/relations.js
+++ models/relations.js
@@ -3,7 +3,7 @@ import Network from "./Network.js";
 import Note from "./Note.js";
 import Favorite from "./Favorite.js";
 import Contact from './Contact.js'
-NetworkContact
+import NetworkContact from './NetworkContact.js'
 
 User.hasMany(Favorite)
 User.hasMany(Contact)
